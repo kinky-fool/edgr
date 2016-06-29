f0b185e33d51ebd9781186a498dbdf69fbb6400c
kinky-fool <kinky.trees@gmail.com>
2016-06-28 23:41:16 -0500

    Update Sessions.pm; alphabetize functions

--- Sessions.pm
+++ Sessions.pm
@@ -17,6 +17,73 @@ $DEBUG        = 0;
                 );
 @EXPORT_OK    = @EXPORT;
 
+sub change_pace {
+  my $session_file  = shift;
+  my $start_bpm     = shift;
+  my $final_bpm     = shift;
+  my $build_time    = shift;
+  my $peak_time     = shift;
+
+  my $diff_bpm      = abs($start_bpm - $final_bpm);
+  my $avg_bpm       = int(($start_bpm + $final_bpm) / 2);
+
+  my $build_beats   = ceil(($avg_bpm / 60) * $build_time);
+  my $peak_beats    = ceil(($final_bpm / 60) * $peak_time);
+
+  my $actual_build_time = 0;
+  my $actual_peak_time  = 0;
+
+  # Beats per step and an extra beat every nth step
+  my $beat_per_step = 0;
+  my $beat_nth_step = 0;
+
+  if ($diff_bpm > 0) {
+    $beat_per_step = floor($build_beats/$diff_bpm);
+    if ($build_beats % $diff_bpm != 0) {
+      $beat_nth_step = floor($diff_bpm / ($build_beats % $diff_bpm));
+    }
+  }
+
+  if (open my $session_fh,'>>',"$session_file") {
+    printf $session_fh "# start: %s\n", $start_bpm;
+    foreach my $step (0 .. ($diff_bpm-1)) {
+      my $beats = $beat_per_step;
+      if ($beat_nth_step && !($step % $beat_nth_step)) {
+        # Add an extra beat, because this is the nth bpm step
+        $beats++;
+      }
+      if ($beats) {
+        my $bpm = $start_bpm + $step;
+        if ($start_bpm > $final_bpm) {
+          $bpm = $start_bpm - $step;
+        }
+        $actual_build_time += $beats / ($bpm / 60);
+        printf $session_fh "%g %g/4 2/8\n",$beats,$bpm;
+      }
+    }
+
+    printf $session_fh "# build_time: %0.2fs (%gs requested)\n",
+            $actual_build_time, $build_time;
+    printf $session_fh "%g %g/4 2/8\n",$peak_beats,$final_bpm;
+    $actual_peak_time = $peak_beats / ($final_bpm / 60);
+    printf $session_fh "# peak_time: %0.2fs (%gs requested)\n",
+            $actual_peak_time, $peak_time;
+  } else {
+    error_msg("Unable to open $session_file: $!",3);
+  }
+  return $actual_peak_time+$actual_build_time;
+}
+
+sub debug_msg {
+  # Standardize debug messages
+  my $message = shift;
+
+  if ($DEBUG) {
+    printf "DEBUG: %s\n",$message;
+  }
+  return undef;
+}
+
 sub error_msg {
   # Standardize error messages
   my $message = shift;
@@ -32,146 +99,6 @@ sub error_msg {
     exit $err_lvl;
   }
   return undef;
-}
-
-sub debug_msg {
-  # Standardize debug messages
-  my $message = shift;
-
-  if ($DEBUG) {
-    printf "DEBUG: %s\n",$message;
-  }
-  return undef;
-}
-
-sub read_config {
-  # Attempt to open and read in values from specified config file
-  # Returns hash ref containing configuration
-  my $conf_file = shift;
-  my %options   = ();
-
-  if (open my $conf_fh,'<',"$conf_file") {
-    while (<$conf_fh>) {
-      my $line = $_;
-
-      # Remove leading / trailing white-space
-      $line =~ s/^\s+//;
-      $line =~ s/\s+$//;
-
-      # Skip empty and commented lines
-      next if ($line =~ /^$/);
-      next if ($line =~ /^#/);
-      # squish whitespace
-      $line =~ s/\s+/\ /g;
-
-      my ($option,$values) = split(/\ /,$line,2);
-      foreach my $value (split(/:/,$values)) {
-        $value =~ s/~/$ENV{HOME}/;
-        $value =~ s/\$HOME/$ENV{HOME}/i;
-        if (defined $options{$option}) {
-          $options{$option} = join(':',$options{$option},$value);
-        } else {
-          $options{$option} = $value;
-        }
-      }
-    }
-    close $conf_fh;
-  } else {
-    error_msg("Unable to open $conf_file: $!",1);
-  }
-
-  return \%options;
-}
-
-sub write_config {
-  my $config_file = shift;
-  my $config      = shift;
-
-  if (open my $config_fh,'>',"$config_file") {
-    foreach my $option (sort { $a cmp $b } keys %$config) {
-      foreach my $value (sort { $a cmp $b } split(/:/,$$config{$option})) {
-        printf $config_fh "%s %s\n", $option, $value;
-      }
-    }
-  } else {
-    error_msg("Unable to open $config_file: $!",2);
-  }
-
-  return 1;
-}
-
-sub fuzzy {
-  my $number  = shift;
-  my $degree  = shift;
-
-  return $number if ($degree <= 0);
-
-  for (1 .. int(rand($degree))) {
-    # Control how far to deviate from $num
-    my $skew = int(rand(3))+2;
-    # Lean toward 0 or 2 * $num
-    my $lean = int(rand(4))+2;
-    # This is not superflous; the rand($lean) below will favor this direction
-    my $point = 1;
-    # "Flip a coin" to determine the direction of the lean
-    if (int(rand(2))) {
-      $point = -1;
-    }
-
-    my $result = $number;
-
-    for (1 .. int($number)) {
-      if (!int(rand($skew))) {
-        if (int(rand($lean))) {
-          $result += $point;
-        } else {
-          $result += ($point * -1);
-        }
-      }
-    }
-
-    $number = $result;
-  }
-  return $number;
-}
-
-sub plus_or_minus {
-  my $num     = shift;
-
-  my $result  = 0;
-
-  for (1 .. int($num)) {
-    if (int(rand(2))) {
-      $result += int(rand(2));
-    } else {
-      $result -= int(rand(2));
-    }
-  }
-
-  return $result;
-}
-
-sub toggle_bool {
-  my $value = shift;
-  if ($value) {
-    return 0;
-  }
-  return 1;
-}
-
-sub sec_to_human {
-  my $secs = shift;
-  if ($secs >= 365*24*60*60) {
-    return sprintf '%.1f years', $secs/(365+*24*60*60);
-  } elsif ($secs >= 24*60*60) {
-    return sprintf '%.1f days', $secs/(24*60*60);
-  } elsif ($secs >= 60*60) {
-    return sprintf '%.1f hours', $secs/(60*60);
-  } elsif ($secs >= 60) {
-    return sprintf '%.1f minutes', $secs/60;
-  } else {
-    return sprintf '%.1f seconds', $secs;
-  }
 }
 
 sub extend_session {
@@ -300,61 +227,54 @@ sub extend_session {
   return $time_added;
 }
 
-sub change_pace {
-  my $session_file  = shift;
-  my $start_bpm     = shift;
-  my $final_bpm     = shift;
-  my $build_time    = shift;
-  my $peak_time     = shift;
+sub fisher_yates_shuffle {
+  my $array = shift;
+  my $i;
 
-  my $diff_bpm      = abs($start_bpm - $final_bpm);
-  my $avg_bpm       = int(($start_bpm + $final_bpm) / 2);
-
-  my $build_beats   = ceil(($avg_bpm / 60) * $build_time);
-  my $peak_beats    = ceil(($final_bpm / 60) * $peak_time);
-
-  my $actual_build_time = 0;
-  my $actual_peak_time  = 0;
-
-  # Beats per step and an extra beat every nth step
-  my $beat_per_step = 0;
-  my $beat_nth_step = 0;
-
-  if ($diff_bpm > 0) {
-    $beat_per_step = floor($build_beats/$diff_bpm);
-    if ($build_beats % $diff_bpm != 0) {
-      $beat_nth_step = floor($diff_bpm / ($build_beats % $diff_bpm));
-    }
+  if (@$array < 2) {
+    return;
   }
 
-  if (open my $session_fh,'>>',"$session_file") {
-    printf $session_fh "# start: %s\n", $start_bpm;
-    foreach my $step (0 .. ($diff_bpm-1)) {
-      my $beats = $beat_per_step;
-      if ($beat_nth_step && !($step % $beat_nth_step)) {
-        # Add an extra beat, because this is the nth bpm step
-        $beats++;
-      }
-      if ($beats) {
-        my $bpm = $start_bpm + $step;
-        if ($start_bpm > $final_bpm) {
-          $bpm = $start_bpm - $step;
+  for ($i = @$array; --$i; ) {
+    my $j = int rand ($i+1);
+    next if $i == $j;
+    @$array[$i,$j] = @$array[$j,$i];
+  }
+}
+
+sub fuzzy {
+  my $number  = shift;
+  my $degree  = shift;
+
+  return $number if ($degree <= 0);
+
+  for (1 .. int(rand($degree))) {
+    # Control how far to deviate from $num
+    my $skew = int(rand(3))+2;
+    # Lean toward 0 or 2 * $num
+    my $lean = int(rand(4))+2;
+    # This is not superflous; the rand($lean) below will favor this direction
+    my $point = 1;
+    # "Flip a coin" to determine the direction of the lean
+    if (int(rand(2))) {
+      $point = -1;
+    }
+
+    my $result = $number;
+
+    for (1 .. int($number)) {
+      if (!int(rand($skew))) {
+        if (int(rand($lean))) {
+          $result += $point;
+        } else {
+          $result += ($point * -1);
         }
-        $actual_build_time += $beats / ($bpm / 60);
-        printf $session_fh "%g %g/4 2/8\n",$beats,$bpm;
       }
     }
 
-    printf $session_fh "# build_time: %0.2fs (%gs requested)\n",
-            $actual_build_time, $build_time;
-    printf $session_fh "%g %g/4 2/8\n",$peak_beats,$final_bpm;
-    $actual_peak_time = $peak_beats / ($final_bpm / 60);
-    printf $session_fh "# peak_time: %0.2fs (%gs requested)\n",
-            $actual_peak_time, $peak_time;
-  } else {
-    error_msg("Unable to open $session_file: $!",3);
+    $number = $result;
   }
-  return $actual_peak_time+$actual_build_time;
+  return $number;
 }
 
 sub init_state {
@@ -439,19 +359,99 @@ sub init_state {
   return $state;
 }
 
-sub fisher_yates_shuffle {
-  my $array = shift;
-  my $i;
+sub plus_or_minus {
+  my $num     = shift;
 
-  if (@$array < 2) {
-    return;
+  my $result  = 0;
+
+  for (1 .. int($num)) {
+    if (int(rand(2))) {
+      $result += int(rand(2));
+    } else {
+      $result -= int(rand(2));
+    }
   }
 
-  for ($i = @$array; --$i; ) {
-    my $j = int rand ($i+1);
-    next if $i == $j;
-    @$array[$i,$j] = @$array[$j,$i];
+  return $result;
+}
+
+sub read_config {
+  # Attempt to open and read in values from specified config file
+  # Returns hash ref containing configuration
+  my $conf_file = shift;
+  my %options   = ();
+
+  if (open my $conf_fh,'<',"$conf_file") {
+    while (<$conf_fh>) {
+      my $line = $_;
+
+      # Remove leading / trailing white-space
+      $line =~ s/^\s+//;
+      $line =~ s/\s+$//;
+
+      # Skip empty and commented lines
+      next if ($line =~ /^$/);
+      next if ($line =~ /^#/);
+      # squish whitespace
+      $line =~ s/\s+/\ /g;
+
+      my ($option,$values) = split(/\ /,$line,2);
+      foreach my $value (split(/:/,$values)) {
+        $value =~ s/~/$ENV{HOME}/;
+        $value =~ s/\$HOME/$ENV{HOME}/i;
+        if (defined $options{$option}) {
+          $options{$option} = join(':',$options{$option},$value);
+        } else {
+          $options{$option} = $value;
+        }
+      }
+    }
+    close $conf_fh;
+  } else {
+    error_msg("Unable to open $conf_file: $!",1);
   }
+
+  return \%options;
+}
+
+sub sec_to_human {
+  my $secs = shift;
+  if ($secs >= 365*24*60*60) {
+    return sprintf '%.1f years', $secs/(365+*24*60*60);
+  } elsif ($secs >= 24*60*60) {
+    return sprintf '%.1f days', $secs/(24*60*60);
+  } elsif ($secs >= 60*60) {
+    return sprintf '%.1f hours', $secs/(60*60);
+  } elsif ($secs >= 60) {
+    return sprintf '%.1f minutes', $secs/60;
+  } else {
+    return sprintf '%.1f seconds', $secs;
+  }
+}
+
+sub toggle_bool {
+  my $value = shift;
+  if ($value) {
+    return 0;
+  }
+  return 1;
+}
+
+sub write_config {
+  my $config_file = shift;
+  my $config      = shift;
+
+  if (open my $config_fh,'>',"$config_file") {
+    foreach my $option (sort { $a cmp $b } keys %$config) {
+      foreach my $value (sort { $a cmp $b } split(/:/,$$config{$option})) {
+        printf $config_fh "%s %s\n", $option, $value;
+      }
+    }
+  } else {
+    error_msg("Unable to open $config_file: $!",2);
+  }
+
+  return 1;
 }
 
 1;
