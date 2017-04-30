c8237ec2a359312bafc52a858c4aace5e59713a5
kinky-fool <kinky.trees@gmail.com>
2017-04-29 21:14:34 -0500

    WIP: I'm wearing PAMPERS!!

--- edgr.pm
+++ edgr.pm
@@ -5,29 +5,19 @@ use DBI;
 use Statistics::Basic qw(:all);
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $DEBUG);
 
-$VERSION      = 0.1;
+$VERSION      = 0.0.1;
 $DEBUG        = 0;
 @ISA          = qw(Exporter);
 # Functions used in scripts
 @EXPORT       = qw(
-                    age_sessions
-                    db_connect
-                    get_user_stats
-                    get_user_times
-                    prompt
+                    read_config
+                    init_session
+                    make_beats
+                    write_script
+                    play_script
                     save_session
                 );
 @EXPORT_OK    = @EXPORT;
-
-sub age_sessions {
-  my $user  = shift;
-  my $dbh = db_connect();
-  my $sql = 'update sessions set ttl=ttl-1 where ttl > 0 and user = ?';
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($user);
-  $sth->finish;
-  $dbh->disconnect;
-}
 
 sub db_connect {
   my $dbf = shift;
@@ -47,13 +37,13 @@ sub error {
 }
 
 sub get_times {
-  # Get an array of session durations for $user_id
-  my $user = shift;
+  my $session = shift;
 
-  my $dbh = db_connect();
-  my $sql = 'select length from sessions where ttl > 0 and user = ?';
+  my $dbh = db_connect($$session{database});
+  my $sql = qq{select length from sessions where user = ? and date between
+    datetime('now', '-6 days') and datetime('now', 'localtime')};
   my $sth = $dbh->prepare($sql);
-  $sth->execute($user);
+  $sth->execute($$session{user});
   my @times = ();
   while (my ($time) = $sth->fetchrow_array) {
     push @times, $time;
@@ -64,12 +54,62 @@ sub get_times {
   return @times;
 }
 
+sub read_config {
+  # Attempt to open and read in values from specified config file
+  # Returns hash ref containing configuration
+  my $conf_file = shift;
+  my %options   = ();
+
+  if (open my $conf_fh,'<',"$conf_file") {
+    while (<$conf_fh>) {
+      my $line = $_;
+
+      # Remove leading / trailing white-space
+      $line =~ s/^\s+//;
+      $line =~ s/\s+$//;
+
+      # Skip empty and commented lines
+      next if ($line =~ /^$/);
+      next if ($line =~ /^#/);
+      # squish whitespace
+      $line =~ s/\s+/\ /g;
+
+      my ($option,$values) = split(/\ /,$line,2);
+      foreach my $value (split(/:/,$values)) {
+        $value =~ s/~/$ENV{HOME}/;
+        $value =~ s/\$HOME/$ENV{HOME}/i;
+        if (defined $options{$option}) {
+          $options{$option} = join(':',$options{$option},$value);
+        } else {
+          $options{$option} = $value;
+        }
+      }
+    }
+    close $conf_fh;
+  } else {
+    error_msg("Err 3: Unable to open $conf_file: $!",1);
+  }
+
+  return \%options;
+}
+
 sub init_session {
   my $conf = shift;
 
   my $session = $conf;
+  my $dbh = db_connect($$session{database});
 
-  update_settings($session);
+  my $sql = 'select * from users where user = ?';
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($$session{user});
+  my $options = $sth->fetchrow_hashref;
+
+  $sth->finish;
+  $dbh->disconnect;
+
+  foreach my $key (keys %$options) {
+    $$session{$key} = $$options{$key};
+  }
 
   $$session{duration} = 0;
 
@@ -84,60 +124,92 @@ sub init_session {
 
   $$session{goal} = $$session{mean} - ($$session{bonus} * $$session{stddev}) +
               rand(($$session{malus} + $$session{bonus}) * $$session{stddev});
+  $$session{time_max} = $$session{goal} * 2;
+
+  update_tempo_limits($session);
+  $$session{bpm_cur} = $$session{bpm_min} +
+            int(rand($$session{bpm_max} - $$session{bpm_min}));
+
+  $$session{direction} = 1;
+  if (int(rand(2))) {
+    $$session{direction} *= -1;
+  }
+
+  $$session{lube_next} = $$session{lube_break};
+  $$session{pattern_chance} = $$session{pattern_reset};
 
   return $session;
 }
 
-sub make_script {
+sub maybe_add_command {
   my $session = shift;
 
-  my $time_max = $$session{goal} * 2;
+  my $command = undef;
 
-  while ($time_max > $$session{duration}) {
-    make_beats($session);
-    # Update sessions between generation, in case it's been changed
-    update_settings($session);
+  if ($$session{duration} > $$session{lube_next}) {
+    if (!int(rand($$session{lube_chance}))) {
+      $command = "Use lube";
+      $$session{lube_next} = $$session{duration} + $$session{lube_break};
+    }
   }
-}
 
-sub play {
-  my $session = shift;
-
-  make_script($session);
-}
-
-sub update_settings {
-  my $session = shift;
-
-  my $dbh = db_connect($$session{database});
-
-  my $sql = 'select * from users where user = ?';
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($$session{user});
-  my $options = $sth->fetchrow_hashref;
-
-  foreach my $key (keys %$options) {
-    $$session{$key} = $$options{$key};
+  if ($command) {
+    return $command;
   }
+
+  return undef;
 }
 
-sub play_session {
+sub write_script {
   my $session = shift;
 
-  my $command  = "aoss $$session{ctronome} -c 1 -w1 $$session{tick_file} ";
-     $command .= "-w2 $$session{tock_file} -p $$session{session_script}";
-  if (open my $metronome_pipe,'-|',"$command 2>/dev/null") {
-    local $SIG{HUP} = sub { close $metronome_pipe; exit 0 };
-    while (my $line = <$metronome_pipe>) {
-      chomp $line;
-      if ($line =~ /^# cmd:(.*)$/) {
-        printf "%s\n" "$1";
+  if (open my $script_fh,'>',$$session{script_file}) {
+    foreach my $beat (split(/#/,$$session{beats})) {
+      my ($count,$bpm) = split(/:/,$beat);
+      while ($count > 0) {
+        if (my $command = maybe_add_command($session)) {
+          printf $script_fh "# %s\n", $command;
+          #printf "# %s\n", $command;
+        }
+        $$session{duration} += 60  / $bpm;
+        printf $script_fh "1 %g/4 2/8\n", $bpm;
+        #printf "1 %g/4 2/8\n", $bpm;
+        $count--;
       }
     }
-    close $metronome_pipe;
+    close $script_fh;
   } else {
-    error("Unable to open pipe: $!",3);
+    error("Unable to open script ($$session{script_file}): $!",1);
   }
+}
+
+#sub play_script {
+#  my $session = shift;
+#
+#  my $command  = "aoss $$session{ctronome} -c 1 -w1 $$session{tick_file} ";
+#     $command .= "-w2 $$session{tock_file} -p $$session{script_file}";
+#  if (open my $metronome_pipe,'-|',"$command 2>/dev/null") {
+#    local $SIG{HUP} = sub { close $metronome_pipe; exit 0 };
+#    while (my $line = <$metronome_pipe>) {
+#      chomp $line;
+#      if ($line =~ /^# (.*)$/) {
+#        printf "%s\n", "$1";
+#      }
+#    }
+#    close $metronome_pipe;
+#  } else {
+#    error("Unable to open pipe: $!",3);
+#  }
+#}
+
+sub play_script {
+  my $session = shift;
+
+  my $start = time();
+  my @args = ("aoss","$$session{ctronome}","-c1", "-w1", "$$session{tick_file}",
+              "-w2", "$$session{tock_file}", "-p", "$$session{script_file}");
+  system(@args);
+  $$session{endured} = abs($start - time());
 }
 
 sub tempo_mod {
@@ -146,17 +218,23 @@ sub tempo_mod {
   my $bpbpm     = shift;
   my $beats_end = shift;
 
-  my $bpm_cur   = $$session{bpm_cur};
-  $$session{duration} += $beats_end / ($bpm_end / 60);
+  my $bpm_cur = $$session{bpm_cur};
 
   # Carry-over beats, to support 0.5 beats per bpm, etc.
   my $beats = 0;
-  for (0 .. abs($bpm_cur - $bpm_end)) {
+
+  # Fix direction if it is broken
+  $$session{direction} = 1;
+  if ($bpm_cur > $bpm_end) {
+    $$session{direction} = -1;
+  }
+
+  while (int($bpm_cur) != int($bpm_end)) {
     $beats += $bpbpm;
-    if ($beats >= 1) {
+    if (int($beats) > 0) {
       $$session{beats} = join('#',(split(/#/,$$session{beats}),
                           sprintf('%g:%g', int($beats), $bpm_cur)));
-      $$session{duration} += int($beats) / ($bpm_cur / 60);
+      $$session{duration} += int($beats) * 60 / $bpm_cur;
       $beats -= int($beats);
     }
     $bpm_cur += $$session{direction};
@@ -164,17 +242,47 @@ sub tempo_mod {
 
   $$session{beats} = join('#',(split(/#/,$$session{beats}),
                                     "$beats_end:$bpm_end"));
+  $$session{duration} += $beats_end * 60 / $bpm_end;
   $$session{bpm_cur} = $bpm_end;
 }
 
 sub update_tempo_limits {
   my $session = shift;
 
-  
+  my $min_range = $$session{bpm_min_max} - $$session{bpm_min_min};
+  my $max_range = $$session{bpm_max_max} - $$session{bpm_max_min};
 
+  # duration from 0 to 1/4 maximum time
+  my $time = $$session{duration};
+  my $percent = $time / ($$session{time_max} / 4);
+  my $min_add = (3/8) * $min_range * $percent;
+  my $max_add = (1/8) * $max_range * $percent;
+
+  if ($$session{duration} >= $$session{time_max} / 4) {
+    $time = $$session{duration} - ($$session{time_max} / 4);
+    $percent = $time / ($$session{time_max} / 4);
+    $min_add = (3/8) * $min_range + ((1/8) * $min_range * $percent);
+    $max_add = (1/8) * $max_range + ((3/8) * $max_range * $percent);
+  }
+
+  if ($$session{duration} >= $$session{time_max} / 2) {
+    $time = $$session{duration} - ($$session{time_max} / 2);
+    $percent = $time / ($$session{time_max} / 4);
+    $min_add = ($min_range / 2) + ((1/8) * $min_range * $percent);
+    $max_add = ($max_range / 2) + ((3/8) * $max_range * $percent);
+  }
+
+  if ($$session{duration} >= $$session{time_max} * 3 / 4) {
+    $time = $$session{duration} - ($$session{time_max} * 3 / 4);
+    $percent = $time / ($$session{time_max} / 4);
+    $min_add = (5/8) * $min_range + ((3/8) * $min_range * $percent);
+    $max_add = (7/8) * $max_range + ((1/8) * $max_range * $percent);
+  }
+
+  $$session{bpm_min} = $$session{bpm_min_min} + $min_add;
+  $$session{bpm_max} = $$session{bpm_max_min} + $max_add;
 }
 
-# mod_tempo(start_bpm,end_bpm,dtime,stime)
 sub standard_segment {
   my $session   = shift;
 
@@ -182,19 +290,30 @@ sub standard_segment {
 
   my $bpm_range = $$session{bpm_max} - $$session{bpm_min};
   my $bpm_mid   = $$session{bpm_min} + $bpm_range / 2;
-  my $bpm_pct   = abs($$session{bpm_cur} - $bpm_mid) /
-                        ($$session{bpm_range} / 2);
+  my $bpm_pct   = abs($$session{bpm_cur} - $bpm_mid) / ($bpm_range / 2);
 
-  my $bpm_delta  = (1 - $bpm_pct) * 20;
-  my $bpm_end = $$session{bpm_cur} + $bpm_delta * $$session{direction};
-  my $bpm_avg = ($bpm_cur + $bpm_end) / 2;
-  my $beats_delta = $time_delta * ($bpm_avg / 60);
+  my $bpm_delta = (((1 - $bpm_pct) * 28) + 2);
+
+  my $bpm_end = $$session{bpm_cur} + ($bpm_delta * $$session{direction});
+
+  if ($bpm_end > $$session{bpm_max}) {
+    $bpm_end = $$session{bpm_max};
+  }
+  if ($bpm_end < $$session{bpm_min}) {
+    $bpm_end = $$session{bpm_min};
+  }
+
+  my $bpm_avg = ($$session{bpm_cur} + $bpm_end) / 2;
+
+  my $time_end    = ($bpm_pct * 18) + 2;
+  my $beats_end   = int(($time_end * $bpm_end) / 60);
+
+  my $time_delta  = ((1 - $bpm_pct) * 18) + 2;
+  my $beats_delta = int(($time_delta * $bpm_avg) / 60);
+
   my $bpbpm = $beats_delta / $bpm_delta;
 
-  my $time_end    = $bpm_pct * 20;
-  my $beats_end   = int($time_end * ($bpm_end / 60));
-
-  tempo_mod($session,$bpm_end,$bpbpm,$beats_end);
+  tempo_mod($session,int($bpm_end),$bpbpm,$beats_end);
 }
 
 sub tempo_jump {
@@ -205,9 +324,16 @@ sub tempo_jump {
 
   my $bpm_range = $$session{bpm_max} - $$session{bpm_min};
   my $bpm_end   = $$session{bpm_min} + ($bpm_range * ($percent / 100));
-  my $bpm_delta = abs($$state{bpm_cur} - $bpm_end);
 
-  tempo_mod($session,$bpm_end,rand(2),int(rand(20))+5);
+  if ($bpm_end > $$session{bpm_max}) {
+    $bpm_end = $$session{bpm_max};
+  }
+
+  if ($bpm_end < $$session{bpm_min}) {
+    $bpm_end = $$session{bpm_min};
+  }
+
+  tempo_mod($session,int($bpm_end),rand(2),int(rand(20))+5);
 }
 
 sub pattern_segment {
@@ -224,7 +350,7 @@ sub pattern_segment {
       tempo_jump($session,  0 + rand(20));
     }
   } else {
-    for (0 .. int(rand(3)) + 3) {
+    for (0 .. int(rand(3)) + 2) {
       standard_segment($session);
     }
   }
@@ -262,48 +388,19 @@ sub make_beats {
   } else {
     $$session{pattern_chance}--;
   }
-
-  return $$session{duration};
-}
-
-sub make_session {
-  my $user_id = shift;
-
-  my $session = get_user_settings($user_id);
-  ($$session{mean},$$session{std_dev}) = get_user_stats($user_id);
-
-  $$session{goal} = $$session{mean}
-      - $$session{deviations} * $$session{std_dev}
-      + rand($$session{deviations} * 2 * $$session{std_dev});
-
-  make_instructions($session)
-  my $session = make_session($user_id,$goal);
-  my $pass = play_session($session);
-}
-
-sub prompt {
-  my $prompt = shift;
-  print "$prompt: ";
-  my $input = <STDIN>;
-  chomp($input);
-  return ($input);
 }
 
 sub save_session {
-  my $user_id = shift;
-  my $ttl     = shift;
-  my $length  = shift;
-  my $goal    = shift;
-  my $mean    = shift;
-  my $fails   = shift;
+  my $session = shift;
 
-  my $dbh = db_connect();
-  my $sql  = 'insert into sessions ';
-     $sql .= '(user_id,ttl,length,goal,mean,fails) ';
-     $sql .= 'values (?,?,?,?,?)';
+  my $dbh = db_connect($$session{database});
+  my $sql  = qq{insert into sessions (user,length,goal,mean,prize_enabled)
+                  values (?,?,?,?,?)};
   my $sth = $dbh->prepare($sql);
 
-  $sth->execute($user_id,$ttl,$length,$goal,$mean,$fails);
+  $sth->execute($$session{user},$$session{endured},$$session{goal},
+                  $$session{mean},$$session{prize_enabled});
   $sth->finish;
   $dbh->disconnect;
+  return;
 }
