3083898e1e60365ebf49c4ab8ecea84352d5aaa8
kinky-fool <kinky.trees@gmail.com>
2017-05-13 14:32:11 -0500

    Change lib capitalization
    I see now why people prefer capitalizing the module / lib. It makes it
    easier to differentiate when typing.

--- Edgr.pm
+++ Edgr.pm
@@ -1,4 +1,4 @@
-package edgr;
+package Edgr;
 
 use strict;
 use DBI;
@@ -43,11 +43,12 @@ sub get_times {
   my $sql = qq{
 select length from sessions where user = ? and
 length / goal > ? / 100 and
-date between datetime('now', '-14 days') and datetime('now', 'localtime')
+date between datetime('now', ?) and datetime('now', 'localtime')
 order by date desc limit ?
   };
   my $sth = $dbh->prepare($sql);
-  $sth->execute($$session{user},$$session{min_pct},$$session{past_sessions});
+  $sth->execute($$session{user},$$session{min_pct},
+                $$session{past_time},$$session{past_sessions});
   my @times = ();
   while (my ($time) = $sth->fetchrow_array) {
     push @times, $time;
